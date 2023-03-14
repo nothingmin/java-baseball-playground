3e60c68486e869cfc668d07a65364c456734d870
unknown <wnl1012@naver.com>
2023-03-14 22:12:49 +0900

    implement string study test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,10 @@
 package study;
 
+import org.assertj.core.internal.bytebuddy.build.ToStringPlugin;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.as;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -25,4 +26,29 @@ public class StringTest {
         String[] split = given.split(",");
         assertThat(split).containsExactly("1");
     }
+
+    @Test
+    void sub_parenthesis(){
+        String given = "(1,2)";
+        String sub = given.substring(1, given.length() - 1);
+        assertThat(sub).isEqualTo("1,2");
+    }
+
+    @Test
+    void get_index(){
+        String given = "abc";
+        char c = given.charAt(0);
+        assertThat(c).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName(value = "charAt StringIndexOutOfBoundsException 발생")
+    void get_out_of_bound(){
+        String given = "abc";
+
+        assertThatThrownBy(()->{
+            char c = given.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
 }
+
