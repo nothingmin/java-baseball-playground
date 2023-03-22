5ff213cb97d29b401492d9c457e64825433cd007
nothingmin <wnl1012@naver.com>
2023-03-23 01:21:09 +0900

    feat get numbers

---
+++ src/main/java/Input.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Input {
+
+    private final Scanner scanner;
+    public Input(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public ArrayList<Integer> inputNumber(){
+        System.out.print("Input Number : ");
+        String value = scanner.nextLine();
+        return Arrays.stream(value.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public boolean inputExit(){
+        while(true){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ");
+            String value = scanner.nextLine();
+            if (value.length() != 1){
+                continue;
+            }
+            if(value.equals("1")){
+                return false;
+            }
+            if(value.equals("2")){
+                return true;
+            }
+        }
+    }
+}
