989fd5ff49be6dc16687b6c74b3e10897ba3c0df
nothingmin <wnl1012@naver.com>
2023-03-23 01:52:47 +0900

    add function list

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,18 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-        String[] values = value.split(" ");
-        StringCalculator stringCalculator = new StringCalculator(values);
-        int result = stringCalculator.calculate();
-        System.out.println("result = " + result);
+        Input input = new Input(new Scanner(System.in));
+        ResultView resultView = new ResultView();
+        boolean exit = false;
+        while(!exit){
+            Game game = new Game(input, resultView);
+            game.startGame();
+            System.out.println(
+                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            exit = input.inputExit();
+        }
     }
 }
