3b8e0cfec5d9b50265003a7a81bcad835e00bfb0
nothingmin <wnl1012@naver.com>
2023-03-27 03:17:08 +0900

    refactor Game move RandomNumbers to constructor

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,18 +5,19 @@ public class Game {
 
     private final Input input;
     private final ResultView resultView;
+    private final RandomNumbers randomNumbers;
 
     private boolean isCorrect = false;
     public Game(Input input, ResultView resultView) {
         this.input = input;
         this.resultView = resultView;
+        this.randomNumbers= new RandomNumbers();
     }
 
     public void startGame(){
-        RandomNumbers randomNumbers = new RandomNumbers();
         while(!isCorrect) {
             ArrayList<Integer> inputNumbers = input.inputNumbers();
-            ArrayList<Integer> result = randomNumbers.compare(inputNumbers);
+            ArrayList<Integer> result = this.randomNumbers.compare(inputNumbers);
             Integer strike = result.get(0);
             isCorrect = checkCorrect(strike);
             resultView.printResult(result);
